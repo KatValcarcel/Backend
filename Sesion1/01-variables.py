f05e264ae6267818b6e84e3092dd5e181d2c7bc8
Kat <88563916+Lecraclav@users.noreply.github.com>
2021-12-10 19:53:59 -0500

    Agrenaod variables

---
+++ Sesion1/01-variables.py
@@ -0,0 +1,23 @@
+#variable numérica
+numero = 10
+numero2 = 10.5
+
+#variable de texto
+nombre = "Kat"
+html='''<html>
+<p>
+</p>'''
+print('Hola! :)')
+print(type(nombre))
+soltero=False
+print(type(soltero))
+
+#arrays
+arreglo=[10,12,40,'Eduardo',14.5,False,[1,2]]
+
+#Diccionario | JSON
+curso={
+'nombre':'Backend',
+'dificultad':'dificil',
+}
+print(curso['dificultad'])
