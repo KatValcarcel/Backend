b67bae792dc1538ac14a4c02bdc070cbdf3e8afd
Kat Valcarcel <kat.valcarcel@live.com>
2022-02-18 15:38:41 -0500

    :bug: ECONNREFUSED

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import { Producto } from "./models/producto.model.js";
+import { Categoria } from "./models/categoria.model.js";
+import { CategoriaProducto } from "./models/categoria_producto.model.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Servidor conectado exitosamente en el puerto ${PORT}`);
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Base de datos sincronizada exitosamente");
+  } catch (error) {
+    console.log(error);
+  }
 });
