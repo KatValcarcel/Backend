dc1578d638f8f078e3034134ac45a358c3fe24e7
Kat Valcarcel <kat.valcarcel@live.com>
2022-03-05 17:18:04 -0500

    chore: actualizacion

--- citas/core/models.py
+++ citas/core/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from tkinter.tix import COLUMN
 from django import db
 from django.db import models
 
@@ -40,13 +41,14 @@ class PersonaModel(models.Model):
         ('CASADO', 'CASADO'),
         ('VIUDO', 'VIUDO'),
         ('DIVORCIADO', 'DIVORCIADO'),
-        ('COMPLICADO', 'COMPLICADO')
+        ('COMPLICADO', 'COMPLICADO'),
         ('NO_ESPECIFICA', 'NO_ESPECIFICA')
     ]
     personaEstadoCivil = models.CharField(
         choices=opcionesEstadoCivil,
         db_column='estado_civil',
-        default='NO_ESPECIFICA'
+        default='NO_ESPECIFICA',
+        max_length=50
     )
     # imageField guarda la ubicacion, el archivo es guardado dentro del proyecto
     personaFoto = models.ImageField(
@@ -54,3 +56,81 @@ class PersonaModel(models.Model):
         upload_to='personas/',  # almacenamiento dentro del proyecto
         null=True
     )
+
+    # se puede crear una clase dentro de otra clase
+    # clase meta sirve para pasar metadata al padre
+    # https://docs.djangoproject.com/en/4.0/ref/models/options/
+    class Meta:
+        # si no se coloca, coloca el nombre del modelo
+        db_table = 'personas'
+        # para crear una clausula no repetible entre dos o más columnas
+        # unique_together = ['nombre', 'apellido', 'estado_civil']
+
+
+class CitasModel(models.Model):
+    citaId = models.AutoField(
+        primary_key=True,
+        unique=True,
+        null=False,
+        db_column='id'
+    )
+    citaDescripcion = models.TextField(
+        null=False,
+        db_column='descripcion'
+    )
+    citaFecha = models.DateTimeField(
+        db_column='fecha',
+        null=False
+    )
+    citaLatitud = models.FloatField(
+        db_column='latitud',
+        null=False
+    )
+    citaLongitud = models.FloatField(
+        db_column='longitud',
+        null=False
+    )
+    opcionesEstadoCita = [
+        ('ACTIVA', 'ACTIVA'),
+        ('CANCELADA', 'CANCELADA'),
+        ('POSTPUESTA', 'POSTPUESTA')
+    ]
+    citaEstado = models.CharField(
+        choices=opcionesEstadoCita,
+        db_column='estado',
+        null=False,
+        max_length=50
+    )
+
+    createdAt = models.DateTimeField(
+        auto_now_add=True,
+        db_column='created_at'
+    )
+
+    updatedAt = models.DateTimeField(
+        auto_now=True,
+        db_column='updated_at'
+    )
+# relaciones
+    citador = models.ForeignKey(
+        to=PersonaModel,
+        db_column='citador_id',
+        on_delete=models.PROTECT,
+        related_name='personaCitas'
+    )
+    citado = models.ForeignKey(
+        to=PersonaModel,
+        db_column='citado_id',
+        on_delete=models.PROTECT,
+        related_name='personaCitadas'
+    )
+
+    class Meta:
+        # la tabla se llame citas
+        db_table = 'citas'
+        # la fecha debe de ser unica con el citador
+        # la fecha debe de ser unica con el citado
+        # no se indica el nombre de la columna de la base de datos, sino que se indica el nombre del atributo de la clase
+        unique_together = [['citaFecha', 'citador'], ['citaFecha', 'citado']]
+        # ordenamiento sea por la fecha mas proxima (desc)
+        ordering = ['-citaFecha']
--- citas/core/views.py
+++ citas/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# vistas usando servicio REST
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+# hereda de CreateAPIView
+
+
+class PersonasController(CreateAPIView):
+    def post(self):
+        '''Se ejecutará todo lo relacionado con el método POST de este controlador'''
+        return 'Hola amigos'
