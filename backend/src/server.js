2639d1afc3cead82d1cff9aefd8bda53e6119eee
Kat Valcarcel <kat.valcarcel@live.com>
2022-03-05 18:32:17 -0500

    on fire 🔥

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express, { json } from "express";
+
+const app = express();
+// convertir json
+app.use(json());
+const PUERTO = process.env.PORT ?? 3000;
+
+app.get("/api/status", (req, res) => {
+  return res.json({
+    date: new Date(),
+  });
+});
+
+app.listen(PUERTO, () => {
+  console.log(`Servidor levantado exitosamente en el puerto ${PUERTO}`);
+});
