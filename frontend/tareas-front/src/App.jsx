08ea9dc3f6268a037425cad6e04c179765d65dfd
Kat Valcarcel <kat.valcarcel@live.com>
2022-03-05 21:20:26 -0500

    app, index y login views

--- frontend/tareas-front/src/App.jsx
+++ frontend/tareas-front/src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { getServerInfo } from "./services/user.service";
-import { Register } from "./views/Register";
 import { Index } from "./views/Index";
+import { Register } from "./views/Register";
+import { Login } from "./views/Login";
 
 function App() {
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Index />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Router>;
 }
--- frontend/tareas-front/src/views/Index.jsx
+++ frontend/tareas-front/src/views/Index.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 export const Index = () => {
+    const navigate = useNavigate()
+
+    const login = (e) => {
+        navigate('/login')
+    }
+
     return (
-        <div>Bienvenido al front de las tareas 😬</div>
+        <div>
+            <p>
+                Bienvenido al front de las tareas 😬
+            </p>
+            <p>
+                ¿Tienes cuenta?
+                <button onClick={login}>Inicia Sesión</button>
+                <Link to='/register'>Crea una nueva cuenta</Link>
+            </p>
+        </div>
+
     )
 }
