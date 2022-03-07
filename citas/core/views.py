278818506e5380c2a4704cbc0310725d2d1f6223
Kat Valcarcel <kat.valcarcel@live.com>
2022-03-06 23:03:01 -0500

    finished django

--- citas/core/views.py
+++ citas/core/views.py
@@ -1,10 +1,12 @@
 # vistas usando servicio REST
 from rest_framework.generics import CreateAPIView
+from .serializers import PersonasSerializer
+from rest_framework.response import Response
 
 # hereda de CreateAPIView
-
-
 class PersonasController(CreateAPIView):
-    def post(self):
+    serializer_class=PersonasSerializer
+    def post(self, request):
+        print(request)
         '''Se ejecutará todo lo relacionado con el método POST de este controlador'''
-        return 'Hola amigos'
+        return Response(data='Hola amigos')
